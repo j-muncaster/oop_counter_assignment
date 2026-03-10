af9e3d6e975eef1f7430fea2cd9bb4b12e2c0dc9
Jo Muncaster <j_muncaster208947@fanshaweonline.ca>
2026-03-10 16:47:04 -0400

    Add: counter.js
    In this commit, the counter.js was added and all of the functional javascript for the counters was written out.

---
+++ js/counter.js
@@ -0,0 +1,69 @@
+export class Counter {
+    constructor(selector, initialValue = 0) {
+        this.count = initialValue;
+        this.selector = selector;
+        this.mount();
+    }
+
+    mount() {
+        const container = document.querySelector(this.selector);
+
+        this.display = document.createElement("div");
+        this.incrementBtn = document.createElement("button");
+        this.decrementBtn = document.createElement("button");
+        this.resetBtn = document.createElement("button");
+
+        this.incrementBtn.textContent = "Increment";
+        this.decrementBtn.textContent = "Decrement";
+        this.resetBtn.textContent = "Reset";
+
+        container.appendChild(this.display);
+        container.appendChild(this.incrementBtn);
+        container.appendChild(this.decrementBtn);
+        container.appendChild(this.resetBtn);
+
+        this.incrementBtn.addEventListener("click", () => this.increment());
+        this.decrementBtn.addEventListener("click", () => this.decrement());
+        this.resetBtn.addEventListener("click", () => this.reset());
+
+        this.update();
+    }
+
+    increment() {
+        this.count++;
+        this.update();
+    }
+
+    decrement() {
+        if (this.count > 0) {
+            this.count--;
+            this.update();
+        }
+    }
+
+    reset() {
+        this.count = 0;
+        this.update();
+    }
+
+    update() {
+        this.display.textContent = `Count: ${this.count}`;
+    }
+}
+
+class StepCounter extends Counter {
+    constructor(selector, initialValue = 0, step = 1) {
+        super(selector, initialValue);
+        this.step = step;
+    }
+
+    increment() {
+        this.count += this.step;
+        this.update();
+    }
+
+    decrement() {
+        this.count -= this.step;
+        this.update();
+    }
+}
